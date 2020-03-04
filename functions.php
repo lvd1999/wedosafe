48d76c73ac38b180566948b460746d073e0861d3
Lee <patricklee0221@gmail.com>
2020-03-04 14:15:15 +0000

    more function methods (register sites, requests)

--- functions.php
+++ functions.php
@@ -9,6 +9,17 @@ function get_details($email)
     return $row;
 }
 
+function get_company($username)
+{
+    global $pdo;
+    $stm = $pdo->prepare("SELECT company_name FROM admins WHERE username = ?");
+    $stm->bindValue(1, $username);
+    $stm->execute();
+    $row = $stm->fetch(PDO::FETCH_ASSOC);
+    $result = $row['company_name'];
+    return $result;
+}
+
 function get_safepass($email)
 {
     global $pdo;
@@ -41,10 +52,62 @@ function get_cert($email)
 function get_pendingrequest($username)
 {
     global $pdo;
-    $stm = $pdo->prepare("SELECT users.firstname, users.surname, bs.address, r.message FROM (((users INNER JOIN requests r ON users.email = r.email) INNER JOIN building_sites bs ON r.code = bs.code) INNER JOIN admins a ON bs.company_name = a.company_name WHERE a.username = ? AND r.status='pending')");
+    $stm = $pdo->prepare("SELECT users.firstname, users.surname, bs.id as bs_id, bs.address, r.message, r.code, r.id, r.email FROM (((users INNER JOIN requests r ON users.email = r.email) INNER JOIN building_sites bs ON r.code = bs.code) INNER JOIN admins a ON bs.company_name = a.company_name) WHERE a.username = ? AND r.status='pending'");
     $stm->bindValue(1, $username);
     $stm->execute();
     $row = $stm->fetchAll(PDO::FETCH_ASSOC);
     return $row;
 }
 
+function get_sites($company_name)
+{
+    global $pdo;
+    $stm = $pdo->prepare("SELECT * FROM building_sites WHERE company_name = ?");
+    $stm->bindValue(1, $company_name);
+    $stm->execute();
+    $row = $stm->fetchAll(PDO::FETCH_ASSOC);
+    return $row;
+}
+
+function codeExists($code) {
+
+    global $pdo;
+    $stm = $pdo->prepare("SELECT * FROM building_sites WHERE code = ?");
+    $stm->bindValue(1, $code);
+    $stm->execute();
+    $row = $stm->fetchAll(PDO::FETCH_ASSOC);
+    if(empty($row)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function acceptRequest($id, $email, $building_site_id) {
+    global $pdo;
+    $stm = $pdo->prepare("UPDATE requests SET status='allowed' WHERE id = ?");
+    $stm->bindValue(1, $id);
+    $stm->execute();
+    $stm2 = $pdo->prepare("INSERT INTO site_registration (email, building_site) VALUES (? , ?)");
+    $stm2->bindValue(1, $email);
+    $stm2->bindValue(2, $building_site_id);
+    $stm2->execute();
+}
+
+function displayRegisteredSites($email) {
+    global $pdo;
+    $stm = $pdo->prepare("SELECT bs.code, bs.address FROM (building_sites bs INNER JOIN requests r ON bs.code = r.code) WHERE r.email = ? AND r.status='allowed'");
+    $stm->bindValue(1, $email);
+    $stm->execute();
+    $row = $stm->fetchAll(PDO::FETCH_ASSOC);
+    return $row;
+}
+
+function siteMembers($id) {
+    global $pdo;
+    $stm = $pdo->prepare("SELECT * FROM site_registration WHERE building_site = ?");
+    $stm->bindValue(1, $id);
+    $stm->execute();
+    $row = $stm->fetchAll(PDO::FETCH_ASSOC);
+    return $row;
+}
