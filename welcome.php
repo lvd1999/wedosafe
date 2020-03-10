44723986aac118b283fa6e1101adb7ee4640ba51
Lee <patricklee0221@gmail.com>
2020-03-10 10:54:07 +0000

    better visualisation on user homepage

--- welcome.php
+++ welcome.php
@@ -11,40 +11,63 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 }
 
 $email = $_SESSION['email'];
-$registeredSites = displayRegisteredSites($email);
+$registeredSites = registeredSites($email);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>User Homepage</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity=   q"sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <style type="text/css">
-        body{ font: 14px sans-serif; text-align: center; }
+        body {
+            font: 14px sans-serif;
+            text-align: center;
+        }
     </style>
 </head>
+
 <body>
     <div class="page-header">
         <h1>Home</h1>
     </div>
     <p>
         <a href="profile/profile.php" class="btn btn-success">Profile</a>
-        <a href="register-site.php" class="btn btn-info">Register a site</a>
-        <a href="../reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="request.php" class="btn btn-info">Enter site code</a>
+        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
     </p>
- 
-    <h2>Your registered sites¬</h2>
-    <?php
-    if(count($registeredSites) > 0)
-    {
-        foreach($registeredSites as $registeredSite) {
-            echo $registeredSite['code'] . "\t" . $registeredSite['address'] . "<br>";
-        }
-    } else {
-        echo "No registered site.";
+
+    <h2>Your registered sites</h2>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">Site Code</th>
+            <th scope="col">Address</th>
+            <th scope="col">Status</th>
+        </tr>
+        </thead>
+       <tbody>
+        <?php
+if (count($registeredSites) > 0) {
+    foreach ($registeredSites as $registeredSite) {
+        echo "<tr scope='row'><td>" . $registeredSite['code'] . "</td>" .            
+            "<td>" . $registeredSite['address'] . "</td>" . 
+            '<td>' . $registeredSite['status'] . '</td>' . 
+            "</tr>";
     }
-    ?>
+} else {
+    echo "No registered site.";
+}
+?>
+    </tbody>
+    </table>
+
 </body>
+
+
+<!-- <embed src="dummy.pdf" width="500" height="375"> -->
+
 </html>
