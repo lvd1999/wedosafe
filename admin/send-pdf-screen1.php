2e969edaac7400c06b3144410d460c8a871abb29
Lee <patricklee0221@gmail.com>
2020-04-08 20:13:14 +0100

    member selection while sending pdf

--- admin/send-pdf-screen1.php
+++ admin/send-pdf-screen1.php
@@ -15,20 +15,39 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+    
     <title>Send PDF screen1</title>
 </head>
 <body>
-    <h3>Select PDF</h3>
+    
+
+    <h1>Select PDF</h1>
 
     <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
     <?php
         foreach($pdfs as $pdf) {
-            echo '<input type="checkbox" id="vehicle1" name="pdf[]" value="'.$pdf['id'].'">
-            <label for="vehicle1">'.$pdf['title'].'</label><br>';
+            echo '<input type="checkbox" id="site" name="pdf[]" value="'.$pdf['id'].'">
+            <label for="site">'.$pdf['title'].'</label><br>';
         }
     ?>
     
-    <button type="submit">Next</button>
+    <button type="submit" id="checkBtn">Next</button>
     </form>
 </body>
 </html>
+
+        <!-- check at least one box -->
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#checkBtn').click(function() {
+        checked = $("input[type=checkbox]:checked").length;
+
+        if(!checked) {
+            alert("You must check at least one checkbox.");
+            return false;
+        }
+
+        });
+    });
+    </script>
